549d5fb731a9c40956eda098c90c53e93945298e
LukeHSM <lucashsmartins94@gmail.com>
2026-05-21 02:39:23 -0300

    Definindo a entidade NinjaModel para o banco de dados.

---
+++ src/main/java/dev/lucasmartins/CadastroDeNinjas/NinjaModel.java
@@ -0,0 +1,24 @@
+package dev.lucasmartins.CadastroDeNinjas;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_cadastro")
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String nome;
+    String email;
+    int idade;
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+}
