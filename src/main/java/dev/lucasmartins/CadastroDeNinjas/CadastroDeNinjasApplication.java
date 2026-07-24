d16fa8afe23520a07edc770470afa0ea3f8c54df
LukeHSM <lucashsmartins94@gmail.com>
2026-07-23 21:22:35 -0300

    fix: Alterações mínimas

--- src/main/java/dev/lucasmartins/CadastroDeNinjas/CadastroDeNinjasApplication.java
+++ src/main/java/dev/lucasmartins/CadastroDeNinjas/CadastroDeNinjasApplication.java
@@ -2,9 +2,8 @@ package dev.lucasmartins.CadastroDeNinjas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class CadastroDeNinjasApplication {
 
 	public static void main(String[] args) {
