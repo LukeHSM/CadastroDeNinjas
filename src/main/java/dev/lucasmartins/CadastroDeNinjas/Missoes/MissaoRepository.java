b8e262c4f71b64bf0a9a1bbb94b3436df397108a
LukeHSM <lucashsmartins94@gmail.com>
2026-08-18 20:22:30 -0300

    refactor: Altera coisas mínimas (ex. apagar linhas em branco)

--- src/main/java/dev/lucasmartins/CadastroDeNinjas/Missoes/MissaoRepository.java
+++ src/main/java/dev/lucasmartins/CadastroDeNinjas/Missoes/MissaoRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 // JpaRepository será uma abstração para fazer as Queries no banco de dados
 // o tipo é Long porque é o tipo do atributo 'id' da MissaoModel
 
-public interface MissoesRepository extends JpaRepository<MissaoModel, Long> {
+public interface MissaoRepository extends JpaRepository<MissaoModel, Long> {
 }
--- src/main/java/dev/lucasmartins/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/lucasmartins/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -15,16 +15,16 @@ public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_ninja")
+    @Column(name = "id")
     private Long id;
 
-    @Column(name = "nome_ninja")
+    @Column(name = "nome")
     private String nome;
 
     @Column(unique = true)
     private String email;
 
-    @Column(name = "idade_ninja")
+    @Column(name = "idade")
     private int idade;
 
     @ManyToOne
